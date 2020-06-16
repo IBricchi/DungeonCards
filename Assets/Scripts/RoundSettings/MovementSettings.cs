e7251d40bde85f3e93051a4159ffec24b5346820
IBricchi <ignacio.bricchi@gmail.com>
2020-06-16 14:33:51 +0200

    Add basic movement settings

---
+++ Assets/Scripts/RoundSettings/MovementSettings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MovementType{
+	public float maxSpeed;
+	public float acceleration;
+	public float deceleration;
+
+	public MovementType(float _maxSpeed, float _acceleartion, float _deceleration){
+		maxSpeed = _maxSpeed;
+		acceleration = _acceleartion;
+		deceleration = _deceleration;
+	}
+}
+public class MovementSettings
+{
+	private static MovementType def = new MovementType(20, 10, 20);
+	private static MovementType fast = new MovementType(20, 10, 20);
+
+	public MovementType GetMovement(MovementID id){
+		MovementType outMovement;
+		switch(id){
+			case MovementID.def:
+				outMovement = def;
+				break;
+			case MovementID.fast:
+				outMovement = fast;
+				break;
+			default:
+				Debug.LogWarning("Non supported MovmentID asked for movement type, default returned");
+				outMovement = def;
+				break;
+		}
+		return outMovement;
+	}
+}
---
+++ Assets/Scripts/RoundSettings/SettingsID.cs
@@ -0,0 +1,5 @@
+﻿public enum MovementID{
+	def,
+	fast,
+	ice
+}
